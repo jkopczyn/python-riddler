4255bfa6f5d3a54b4a3b5b8f1526aad5a0aca44f
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-08-20 19:29:54 -0700

    a much faster way to calculate long-run ratios

--- 3332.py
+++ 3332.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 
 class WeirdSequenceCounter(object):
     def __init__(self):
@@ -31,7 +32,41 @@ class WeirdSequenceCounter(object):
                 break
             previous_ratio = new_ratio
 
+class WeirdSequenceExpander(object):
+    def __init__(self, seed="3"):
+        self.count = {3: 0, 2: 0}
+        for i in (int(c) for c in seed if c in '23'):
+            self.count[i] += 1
 
-counter = WeirdSequenceCounter()
-counter.iterate_until_stability()
-import pdb; pdb.set_trace()
+    def expand_once(self):
+        self.count = {
+                3: 3*self.count[3]+2*self.count[2],
+                2: self.count[3]+self.count[2]
+                }
+        return Fraction(self.count[3], self.count[2])
+
+    def expand_n_times(self, n=10):
+        for i in range(n):
+            ratio = self.expand_once()
+        print "ratio: %.70f" % float(ratio)
+        return ratio
+
+    def expand_until_stability(self, n=15, threshold=0.000000000000000001, m=1):
+        previous_ratio = 10
+        for i in range(m):
+            new_ratio = self.expand_n_times(n)
+            if False: #abs(new_ratio - previous_ratio) < threshold:
+                break
+            previous_ratio = new_ratio
+        return new_ratio
+
+
+counter = WeirdSequenceExpander()
+result = counter.expand_until_stability()
+print result
+counter = WeirdSequenceExpander("2")
+result = counter.expand_until_stability()
+print result
+counter = WeirdSequenceExpander("23332222222222222223332233")
+result = counter.expand_until_stability()
+print result
